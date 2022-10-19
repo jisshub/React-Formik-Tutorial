fbad021ab7f7038c518387c52c02ba31a1510866
macmini-m1-2 <jaisonjoseph@macminim12sMini.alphionsee.in>
2022-10-19 17:31:16 +0530

    Form controls

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 
 [Managing Form State](#managing-form-state)
 
+[Handling Form Submission](#handling-form-submission)
+
+[Form Validation](#form-validation)
 # Formik
 
 Formik is a library that helps you deal with forms in React.
@@ -121,11 +124,105 @@ const formik = useFormik({
 
 2. *onSubmit* method receives the latest values of form data as its argument.
  
-# 6. Form validation
+# Form Validation
 
 https://www.youtube.com/watch?v=tthHwRRGkj0&list=PLC3y8-rFHvwiPmFbtzEWjESkqBVDbdgGu&index=6
 
 For every field, we add required attributes. In other all fields are important and neds to implementated,
 
-time: 4:25
+
+- After cleaning the codebase, Here we take *initialValues* property, *onSubmit* and *validate* function outside the main function. In *useFormik* hook, we pass above ones as arguments.
+
+```js
+import React from 'react';
+import {useFormik} from 'formik';
+
+  const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+  }
+
+  const onSubmit = values => {
+    console.log('form data', values)
+  }
+
+  const validate = values => {
+    let errors = {}
+    if (!values.name) {
+        errors.name = 'Required'
+    } if (!values.email) {
+        errors.email = 'Required'
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
+        errors.email = 'Invalid email format';
+    }
+    if (!values.channel) {
+        errors.channel = 'Required';
+    }
+    return errors;
+  }
+  function YoutubeForm() {
+    const formik = useFormik({
+      initialValues,
+      onSubmit,
+      validate
+    });
+  return (
+    <div>
+        <form onSubmit={formik.handleSubmit}>
+            <label htmlFor='name'>Name</label>
+            <input 
+              type="text" 
+              id="name" 
+              name='name'
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
+
+            <label htmlFor='email'>E-mail</label>
+            <input 
+              type={'email'} 
+              id='email' 
+              name='email'
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
+
+            <label htmlFor='channel'>Channel</label>
+            <input 
+              type={'channel'} 
+              id='channel'
+              name='channel'
+              onChange={formik.handleChange}
+              value={formik.values.channel}
+            />
+
+            <button type='submit'>Submit</button>
+        </form>
+    </div>
+  )
+}
+export default YoutubeForm;
+```
+
+# Displaying Error Messages
+
+Display error messages only if it exists. For that we use conditional rendering.
+
+```js
+<input 
+    type="text" 
+    id="name" 
+    name='name'
+    onChange={formik.handleChange}
+    value={formik.values.name}
+  />
+  {formik.errors.name ? 
+    <div>
+      {formik.errors.name}
+    </div>: null
+  }
+```
+
+If the errors exist in name input, we render the div element. Similarly we write error messages for other inputs as well.
 
--- react-formik-demo-app/src/components/YoutubeForm.js
+++ react-formik-demo-app/src/components/YoutubeForm.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import {useFormik} from 'formik';
 
-function YoutubeForm() {
-
   const initialValues = {
-    name: 'Ajith',
+    name: '',
     email: '',
     channel: ''
   }
@@ -13,60 +11,83 @@ function YoutubeForm() {
     console.log('form data', values)
   }
 
-    const formik = useFormik({
-      onSubmit: values => {
-        console.log('form data', values)
-      },
-      validate: values => {
-        // valoues.name values.email values.channel; 
-        let errors = {}
-        if (!values.name) {
-            errors.name = 'Required'
-        } if (!values.email) {
-            errors.email = 'Required'
-        } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
-            errors.email = 'Invalid email format'
-        }
-        if (!values.channel) {
-            errors.channel = 'Required'
-        }
-        return errors;
-      }
+  const validate = values => {
+    let errors = {}
+    if (!values.name) {
+        errors.name = 'Required'
+    } if (!values.email) {
+        errors.email = 'Required'
+    } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
+        errors.email = 'Invalid email format';
+    }
+    if (!values.channel) {
+        errors.channel = 'Required';
+    }
+    return errors;
+  }
 
-  
+  function YoutubeForm() {
+    const formik = useFormik({
+      initialValues,
+      onSubmit,
+      validate
     });
+
+    console.log("form values", formik.errors);
 
   return (
     <div>
         <form onSubmit={formik.handleSubmit}>
+
+          <div className='form-control'>
             <label htmlFor='name'>Name</label>
-            <input 
-              type="text" 
-              id="name" 
-              name='name'
-              onChange={formik.handleChange}
-              value={formik.values.name}
-            />
-
+              <input 
+                type="text" 
+                id="name" 
+                name='name'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+              />
+              {formik.errors.name ? 
+                <div className='error'>
+                  {formik.errors.name}
+                </div>: null
+              }
+          </div>
+            
+          <div className='form-control'>
             <label htmlFor='email'>E-mail</label>
-            <input 
-              type={'email'} 
-              id='email' 
-              name='email'
-              onChange={formik.handleChange}
-              value={formik.values.email}
-            />
-
+              <input 
+                type={'email'} 
+                id='email' 
+                name='email'
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+              {formik.errors.email ? 
+                <div className='error'>
+                  {formik.errors.email}
+                </div>: null
+              }
+          </div>
+          
+          <div className='form-control'>
             <label htmlFor='channel'>Channel</label>
-            <input 
-              type={'channel'} 
-              id='channel'
-              name='channel'
-              onChange={formik.handleChange}
-              value={formik.values.channel}
-            />
-
-            <button type='submit'>Submit</button>
+              <input 
+                type={'channel'} 
+                id='channel'
+                name='channel'
+                onChange={formik.handleChange}
+                value={formik.values.channel}
+              />
+              {formik.errors.channel ? 
+                <div className='error'>
+                  {formik.errors.channel}
+                </div> : null
+              }
+          </div>
+          <button type='submit'>Submit</button>
+          
         </form>
     </div>
   )
